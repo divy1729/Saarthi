63c1af79426460818cfa28eef283caef2a1929d9
Divyansh Saxena <divyanshsaxena1729@gmail.com>
2025-08-15 16:41:43 +0530

    resolved bug

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "lh3.googleusercontent.com", // Google profile photos
+    ],
+  },
   /* config options here */
 };
 
